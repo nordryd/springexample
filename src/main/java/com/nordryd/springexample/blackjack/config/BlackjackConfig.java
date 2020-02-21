6e730f84e779b79db5b261b3b075673ece1788cc
Jake Overton <jake.overton2@cerner.com>
2020-02-21 09:21:00 -0600

    Generalizing Card & starting War game bc it's easier lol

--- src/main/java/com/nordryd/springexample/blackjack/config/BlackjackConfig.java
+++ src/main/java/com/nordryd/springexample/blackjack/config/BlackjackConfig.java
@@ -13,4 +13,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 public class BlackjackConfig
-{}
+{
+
+}
---
+++ src/main/java/com/nordryd/springexample/gameobjects/Card.java
@@ -0,0 +1,14 @@
+package com.nordryd.springexample.gameobjects;
+
+/**
+ * <p>
+ *
+ * </p>
+ * <p>
+ * Copyright &copy; 2020 Cerner Corporation
+ * </p>
+ *
+ * @author Jake Overton
+ */
+public class Card
+{}
